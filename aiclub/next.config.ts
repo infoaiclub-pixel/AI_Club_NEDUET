9f3837b4b3536909449e326d35d7ffcf24e639e8
Haseeb Alvi <haseebalvi91@gmail.com>
2026-06-16 20:08:31 +0500

    allowed image optimization and caching from new cms domain

--- aiclub/next.config.ts
+++ aiclub/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ai-club-neduet-bcvi.vercel.app',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
